5b91dffef69a226d4e67a42110d53bee07917dfd
Raju Chigicherla <crajujava11@gmail.com>
2022-06-30 14:43:59 +0530

    Updated test cases

--- src/test/java/com/forecast/eureka/client/controller/ForecastControllerTest.java
+++ src/test/java/com/forecast/eureka/client/controller/ForecastControllerTest.java
@@ -4,9 +4,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,13 +16,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.forecast.eureka.client.constants.AppConstants;
 import com.forecast.eureka.client.model.WeatherAverageDTO;
 import com.forecast.eureka.client.service.ForecastService;
 
-@ExtendWith(SpringExtension.class)
 @WebMvcTest(ForecastController.class)
 class ForecastControllerTest {
 
@@ -34,7 +34,6 @@ class ForecastControllerTest {
 	MockMvc mockMvc;
 
 	@Test
-	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public void testWeatherForecastAverage() throws Exception {
 
 		var result = new ArrayList<WeatherAverageDTO>();
@@ -45,8 +44,11 @@ class ForecastControllerTest {
 		result.add(obj1);
 		result.add(obj2);
 		result.add(obj3);
+		Map<String, Object> testmap = new HashMap<>();
+		testmap.put(AppConstants.WEATHER_AVERAGE, result);
+		testmap.put(AppConstants.WEATHER_MESSAGE, "Testing");
 
-		ResponseEntity testData = new ResponseEntity<>(result, HttpStatus.OK);
+		ResponseEntity<Map<String, Object>> testData = new ResponseEntity<>(testmap, HttpStatus.OK);
 		Mockito.when(forecastService.weatherForecastAverage(Mockito.anyString())).thenReturn(testData);
 		mockMvc.perform(get("/forecast").param("city", "Bengaluru")).andExpect(status().isOk());
 	}
@@ -55,11 +57,8 @@ class ForecastControllerTest {
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public void testWeatherForecastAverage_1() throws Exception {
 		LOG.info("Checking the City not found exception");
-
 		ResponseEntity testData = new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-
 		Mockito.when(forecastService.weatherForecastAverage(Mockito.anyString())).thenReturn(testData);
-
 		mockMvc.perform(get("/forecast")).andExpect(status().isBadRequest());
 	}
 
